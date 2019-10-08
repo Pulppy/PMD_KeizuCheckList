41831207f527c2e3a9e57314bf4625141558808b
Lu Chi Hao <52203205+laochihu@users.noreply.github.com>
2019-10-08 16:54:16 +0700

    Update general 19

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General19.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General19.java
@@ -3,6 +3,7 @@ package net.sourceforge.pmd.lang.apex.rule.codestyle;
 
 import net.sourceforge.pmd.lang.apex.ast.ASTField;
 import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
+import net.sourceforge.pmd.lang.apex.ast.ASTReferenceExpression;
 import net.sourceforge.pmd.lang.apex.ast.ASTUserClass;
 import net.sourceforge.pmd.lang.apex.ast.ASTVariableExpression;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
@@ -14,6 +15,7 @@ public class General19 extends AbstractApexRule{
 	@Override
 	public Object visit(ASTField node, Object data) {
 		Integer count = 0;
+		Integer count1 = 0;
 		ASTUserClass nodeFather = node.getFirstParentOfType(ASTUserClass.class);
 		List<ASTMethod> lst = nodeFather.findDescendantsOfType(ASTMethod.class);
 		for(ASTMethod ele : lst) {
@@ -27,12 +29,25 @@ public class General19 extends AbstractApexRule{
 						}
 					}
 					if(lstGrandJr != null && lstGrandJr.size() > 0) {
-						count = count + 1;
+						count1 = count1 + 1;
+					}
+				}
+				
+				List<ASTReferenceExpression> lstJr1 = ele.findDescendantsOfType(ASTReferenceExpression.class);
+				if(lstJr1 != null && lstJr1.size() > 0) {
+					List<ASTVariableExpression> lstGrandJr = new ArrayList<>();
+					for(ASTVariableExpression eleJr : lstJr) {
+						if(eleJr.getImage().contentEquals(node.getImage())) {
+							lstGrandJr.add(eleJr);
+						}
+					}
+					if(lstGrandJr != null && lstGrandJr.size() > 0) {
+						count1 = count1 + 1;
 					}
 				}
 			}
 		}
-		if(count <= 1) {
+		if((count < 2 && count > 0) || (count1 < 2 && count1 > 0)) {
 			addViolation(data, node);
 		}
 		return data;
