491c8e97274e205df6d88caefab844652d69c751
Lu Chi Hao <52203205+laochihu@users.noreply.github.com>
2019-10-09 13:22:26 +0700

    Update to 6.18 version

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General02.java
@@ -0,0 +1,29 @@
+package net.sourceforge.pmd.lang.apex.rule.codestyle;
+
+import java.util.List;
+
+import net.sourceforge.pmd.lang.apex.ast.ASTBlockStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
+import net.sourceforge.pmd.lang.apex.ast.ASTTryCatchFinallyBlockStatement;
+
+public class General02 extends AbstractNamingConventionsRule{
+
+	@Override
+	String displayName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Object visit(ASTMethod node, Object data) {
+		if(node.getModifiers().isPublic()) {
+
+			ASTBlockStatement blockStatement = node.findChildrenOfType(ASTBlockStatement.class).get(0);
+			
+			List<ASTTryCatchFinallyBlockStatement> tryCatchBlockList = blockStatement.findDescendantsOfType(ASTTryCatchFinallyBlockStatement.class);
+			if(tryCatchBlockList.isEmpty()) {
+				addViolation(data, node);	
+			}
+		}
+        return data;
+    }
+}
