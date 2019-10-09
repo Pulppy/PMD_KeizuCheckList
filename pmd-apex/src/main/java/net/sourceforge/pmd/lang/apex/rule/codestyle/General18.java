8bac9f028927142cb20a9dcf59dd12a46b5e7f33
Lu Chi Hao <52203205+laochihu@users.noreply.github.com>
2019-10-09 18:11:45 +0700

    Update General18.java

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General18.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General18.java
@@ -1,14 +1,15 @@
 package net.sourceforge.pmd.lang.apex.rule.codestyle;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import net.sourceforge.pmd.lang.apex.ast.ASTAssignmentExpression;
-import net.sourceforge.pmd.lang.apex.ast.ASTExpressionStatement;
+
+import net.sourceforge.pmd.lang.apex.ast.ASTLiteralExpression;
 import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
-import net.sourceforge.pmd.lang.apex.ast.ASTReferenceExpression;
 import net.sourceforge.pmd.lang.apex.ast.ASTVariableExpression;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+import net.sourceforge.pmd.lang.ast.Node;
 /* @class: MethodCallMstHvCommaAndSpaceBtw
  * 
  * @created: KSVC Truong Trang Ngoc Phuc
@@ -19,65 +20,50 @@ import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 public class General18 extends AbstractApexRule{
 	@Override
 	public Object visit(ASTMethodCallExpression node, Object data ) {
-		List<ASTExpressionStatement> expStmtList = node.getParentsOfType(ASTExpressionStatement.class);
-		ASTExpressionStatement expStmt = null;
-		Integer beginCol = 0;
-		Integer endCol = 0;
-		boolean isSingleLine = false;
-		Integer countCol;
-		
-		if (!node.findChildrenOfType(ASTMethodCallExpression.class).isEmpty()) {
-			return data;
+		List<Node> listNode = new ArrayList<>();
+		List<Node> listNode2;
+		List<ASTLiteralExpression> listLit = node.findChildrenOfType(ASTLiteralExpression.class);
+		List<ASTVariableExpression> listVar = node.findChildrenOfType(ASTVariableExpression.class);
+		for(Node ele : listLit) {
+			listNode.add(ele);
 		}
-		
-		if (!node.findChildrenOfType(ASTReferenceExpression.class).isEmpty() ) {
-			return data;
+		for(Node ele : listVar) {
+			listNode.add(ele);
 		}
-		
-		if (node.getFirstParentOfType(ASTAssignmentExpression.class) != null) {
-			return data;
-		}
-		
-		if(!expStmtList.isEmpty()) {
-			expStmt = expStmtList.get(0);
-			isSingleLine = expStmt.isSingleLine();
-			beginCol = expStmt.getBeginColumn();
-			endCol = expStmt.getEndColumn();
-		}
-		countCol = beginCol;
-		
-		if(expStmt != null && isSingleLine) {
-			List<String> paramNameList = new ArrayList<String>();
-			String methodName = node.getFullMethodName();
-			List<ASTVariableExpression> varExpList = node.findChildrenOfType(ASTVariableExpression.class);
-			if (!varExpList.isEmpty()) {
-				for(ASTVariableExpression varExp : varExpList) {
-					paramNameList.add(varExp.getImage());
+		List<Integer> listLine = new ArrayList<Integer>();
+		for(Node ele : listNode) {
+			if(listLine.isEmpty()) {
+				listLine.add(ele.getBeginLine());
+			}else {
+				for(Integer i = 0; i < listLine.size(); i++) {
+					if(listLine.get(i) == ele.getBeginLine()) {
+						break;
+					}else if(i == listLine.size() - 1) {
+						listLine.add(ele.getBeginLine());
+					}
 				}
 			}
-			
-			countCol += methodName.length();
-			if (!paramNameList.isEmpty()) {
-				for (String paramName : paramNameList) {
-					countCol += paramName.length();
+		}
+		for(Integer line : listLine) {
+			listNode2 = new ArrayList<>();
+			for(Node ele : listNode) {
+				if(ele.getBeginLine() == line) {
+					listNode2.add(ele);
 				}
-			} else {
-				return data;
 			}
-			
-			if (!paramNameList.isEmpty()) {
-				countCol += (paramNameList.size() - 1) * 2;	// ' ' + ','
+			for(Integer i = 0; i < listNode2.size() - 1; i++) {
+				for(Integer j = i + 1; j < listNode2.size(); j++) {
+					if(listNode2.get(j).getBeginColumn() < listNode2.get(i).getBeginColumn()) {
+						 Collections.swap(listNode2, i, j);
+					}
+				}
 			}
-			countCol += 2;	// '(' + ')'
-			
-			if (countCol != endCol) {
-				addViolation(data, node, "");
+			for(Integer i = 0; i < listNode2.size() - 1; i++) {
+				if(listNode2.get(i).getEndColumn() + 3 != listNode2.get(i + 1).getBeginColumn()) {
+					addViolation(data, listNode2.get(i + 1));
+				}
 			}
 		}
-		
 		return data;
-	}
-	
-	
-	
+	}	
 }
