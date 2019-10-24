e86a8edf159c2a31af2f98c7863e7d4b21cfb902
ptruong <52196656+tnphuctruong@users.noreply.github.com>
2019-10-24 18:13:31 +0700

    Sửa General 53

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General53.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General53.java
@@ -1,16 +1,49 @@
 package net.sourceforge.pmd.lang.apex.rule.codestyle;
 
+import java.util.List;
+
+import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
 import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
+import net.sourceforge.pmd.lang.apex.ast.ASTNewObjectExpression;
+import net.sourceforge.pmd.lang.apex.ast.ASTReturnStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTVariableDeclaration;
+import net.sourceforge.pmd.lang.apex.ast.ASTVariableExpression;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 
 public class General53 extends AbstractApexRule {
-	private static final String GET_CURRENT_PAGE = "apexPage.getcurrentPage";
+	private static final String GET_CURRENT_PAGE = "apexPage.getcurrentpage";
 	private static final String GET_PARAMETER = "getparameters.get";
 	
 	@Override
+	public Object visit(ASTMethod node, Object data) {
+		if(node.getReturnType().toLowerCase().contentEquals("pagereference")) {
+			List<ASTReturnStatement> returnStmList = node.findChildrenOfType(ASTReturnStatement.class);
+			for (ASTReturnStatement returnStm : returnStmList) {
+				ASTNewObjectExpression newObjExpression = returnStm.getFirstChildOfType(ASTNewObjectExpression.class);
+				ASTVariableExpression varExpression = returnStm.getFirstChildOfType(ASTVariableExpression.class);
+				
+				if(newObjExpression != null) {
+					if(newObjExpression.getType().toLowerCase().contentEquals("pagereference")) {
+						addViolation(data, node);
+					}
+				}
+				if(varExpression != null) {
+					List<ASTVariableDeclaration> varExpList = node.findDescendantsOfType(ASTVariableDeclaration.class);
+					for(ASTVariableDeclaration varExp : varExpList) {
+						if(varExp.getImage().contentEquals(varExpression.getImage()) 
+						 && varExp.getType().toLowerCase().contentEquals("pagereference")) {
+							addViolation(data, varExpression);
+						}
+					}
+				}
+			}
+		}
+		return data;
+	}
+	@Override
 	public Object visit(ASTMethodCallExpression node, Object data) {
 		String methodName = node.getFullMethodName().toLowerCase();
-		if (methodName.contains(GET_CURRENT_PAGE) || methodName.contains(GET_PARAMETER)) {
+		if (methodName.toLowerCase().contains(GET_CURRENT_PAGE) || methodName.toLowerCase().contains(GET_PARAMETER)) {
 			addViolation(data, node);
 		}
 		
