7797753a2096a8970a3d3c11c0708a3e01d3cb6a
ptruong <52196656+tnphuctruong@users.noreply.github.com>
2019-10-25 14:18:10 +0700

    VisualPage-07

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/VisualPage07.java
@@ -0,0 +1,73 @@
+package net.sourceforge.pmd.lang.apex.rule.codestyle;
+
+import java.util.List;
+
+import net.sourceforge.pmd.lang.apex.ast.ASTDmlInsertStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTDmlUpdateStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
+import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
+import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+/*
+ * Class		: VisualPage07
+ * Author		: Truong Trang Ngoc Phuc
+ * Created		: 2019/10/25
+ * Discription	: Trong su ly thuc hien insert, upate DB thi phai su dung savepoint de tao transaction. Truong hop tai lieu ko mo ta thi confirm lai giup.
+ */
+public class VisualPage07 extends AbstractApexRule {
+	@Override
+	public Object visit(ASTMethod node, Object data) {
+		List<ASTDmlInsertStatement> insertStmList = node.findDescendantsOfType(ASTDmlInsertStatement.class);
+		List<ASTDmlUpdateStatement> updateStmList = node.findDescendantsOfType(ASTDmlUpdateStatement.class);
+		// Neu ko co DML trong method thi ket thuc
+		if (insertStmList.isEmpty() && updateStmList.isEmpty()) {
+			return data;
+		}
+		
+		// Kiem tra xem co Database.setSavePoint() va Database.rollback hay ko
+		Integer savepointBeginLine = -1;
+		Integer rollbackBeginLine = -1;
+		boolean hasSetSavePointAndRollback = true;
+		List<ASTMethodCallExpression> methodCallList = node.findDescendantsOfType(ASTMethodCallExpression.class);
+		if (methodCallList.isEmpty()) {
+			addViolation(data, node);
+		} else {
+			for (ASTMethodCallExpression mce :methodCallList) {
+				String fullMethodName = mce.getFullMethodName();
+				if (fullMethodName.toLowerCase().contentEquals("database.setsavepoint")) {
+					savepointBeginLine = mce.getBeginLine();
+				}
+				if (fullMethodName.toLowerCase().contentEquals("database.rollback")) {
+					rollbackBeginLine = mce.getBeginLine();
+				}
+			}
+		}
+		
+		// Neu ko co setSavePoint hoac ko co rollback thi vang loi va ket thuc
+		if (savepointBeginLine == -1 || rollbackBeginLine == -1) {
+			hasSetSavePointAndRollback = false;
+		}
+		
+		// Kiem tra insert
+		if (!insertStmList.isEmpty()) {
+			for (ASTDmlInsertStatement insertStm : insertStmList) {
+				if ( !(insertStm.getBeginLine() >= savepointBeginLine
+						&& insertStm.getBeginLine() <= rollbackBeginLine)
+						|| !hasSetSavePointAndRollback) {
+					addViolation(data, insertStm);
+				}
+			}
+		}
+		
+		// Kiem tra update
+		if (!updateStmList.isEmpty()) {
+			for (ASTDmlUpdateStatement updateStm : updateStmList) {
+				if ( !(updateStm.getBeginLine() >= savepointBeginLine 
+						&& updateStm.getBeginLine() <= rollbackBeginLine)
+						|| !hasSetSavePointAndRollback) {
+					addViolation(data, updateStm);
+				}
+			}
+		}
+		return data;
+	}
+}
