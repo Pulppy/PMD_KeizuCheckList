a073c90307008b39b887725c94c68a24432d1fc6
ptruong <52196656+tnphuctruong@users.noreply.github.com>
2019-10-28 11:52:35 +0700

    Thêm comment VisualPage08

--- pmd-visualforce/src/main/java/net/sourceforge/pmd/lang/vf/rule/VisualPage08.java
+++ pmd-visualforce/src/main/java/net/sourceforge/pmd/lang/vf/rule/VisualPage08.java
@@ -3,13 +3,19 @@ package net.sourceforge.pmd.lang.vf.rule;
 import java.util.List;
 
 import net.sourceforge.pmd.lang.vf.ast.ASTElement;
-
+/*
+ * Class		: VisualPage08
+ * @Created		: 2019/10/28 Truong Trang Ngoc Phuc
+ * @Description	: Khai bao 1 page, 1 form tuong ung. Ko khai bao 2 form cho 1 page
+ * @Modified		:
+ * */
 public class VisualPage08 extends AbstractVfRule {
 	@Override
 	public Object visit(ASTElement node, Object data) {
 		// Tim kiem cac node con
 		List<ASTElement> childNodeList = node.findDescendantsOfType(ASTElement.class);
-		Integer noTagForm = 0;	// So the apex:form
+		// So the apex:form
+		Integer noTagForm = 0;
 		for (ASTElement child : childNodeList) {
 			if (child.getName().toLowerCase().contentEquals("apex:form")) {
 				noTagForm++;
