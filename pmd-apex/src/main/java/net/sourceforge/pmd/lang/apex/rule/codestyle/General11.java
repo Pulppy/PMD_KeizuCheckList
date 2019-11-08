71cf434348d155214fe64cd96c2987df15fb8b75
ptruong <52196656+tnphuctruong@users.noreply.github.com>
2019-11-08 10:41:19 +0700

    [General-11] #Bug248 nhieu message cung 1 loai, dang xuat ra tren 1 dong code
    Trước: add nhiều lỗi cùng loại vào 1 dòng.
    Sau: chỉ add 1 lỗi cùng loại vào 1 dòng

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General11.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codestyle/General11.java
@@ -12,10 +12,17 @@ import net.sourceforge.pmd.lang.apex.ast.ASTVariableDeclarationStatements;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 
 /*
-* @created: Lu Chi Hao
+* @created	: Lu Chi Hao
+* @modified	: 2019/11/08 Truong Trang Ngoc Phuc
 */
 
 public class General11 extends  AbstractApexRule {
+	// List luu nhung line da add loi
+	private static List<Integer> errLineList = new ArrayList() {
+		{
+			add(-1);
+		}
+	};
 	@Override
 	public Object visit(ASTUserClass node, Object data) {
 		List<ASTLiteralExpression> lst0 = node.findDescendantsOfType(ASTLiteralExpression.class);
@@ -63,7 +70,24 @@ public class General11 extends  AbstractApexRule {
 			if(mapLit.get(key).size() > 1) {
 				List<ASTLiteralExpression> lst = mapLit.get(key);
 				for(ASTLiteralExpression ele : lst) {
-					addViolation(data, ele);
+				// 2019/11/08 Truong Trang Ngoc Phuc modify start	
+//					addViolation(data, ele);
+					// flag kiem tra line hien tai da co add loi chua
+					boolean thisLineHasError = false;
+					//  Neu line hien tai da dc add error thi bat co thisLineHasError
+					for (Integer line : errLineList) {
+						if (ele.getBeginLine() == line) {
+							thisLineHasError = true;
+							break;
+						}
+					}
+					// Neu line hien tai chua dc add error thi
+					// add error vao node do va luu line hien tai vao List errLineList
+					if (!thisLineHasError) {
+						addViolation(data, ele);
+						errLineList.add(ele.getBeginLine());
+					}
+				// 2019/11/08 Truong Trang Ngoc Phuc modify end
 				}
 			}
 		}
